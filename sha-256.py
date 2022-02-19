e37a6bcfcc27113e11beb76d96962b72a7a95952
Marvin Odobashi <37641738+MarvOdo@users.noreply.github.com>
2022-02-19 14:10:20 -0500

    Added sha-256.py

---
+++ sha-256.py
@@ -0,0 +1,108 @@
+import math
+
+def toBinary(a):
+	CharList = []
+	for i in range(len(a)):
+		element = str(bin(ord(a[i])))[2:]
+		while len(element) < 8:
+			element = "0" + element
+		CharList.append(element)
+	BString = "".join(CharList)
+	BNum = int(BString, 2)
+	return (BString, BNum)
+
+def rightRotate(n, d, m):
+	return ((n & (2**m-1)) >> d%m) | (n << (m-(d%m)) & (2**m-1))
+
+def sha256(msg:str):
+	OgMessage = msg
+
+	message = toBinary(OgMessage)[1]
+
+	h0 = 0x6a09e667
+	h1 = 0xbb67ae85
+	h2 = 0x3c6ef372
+	h3 = 0xa54ff53a
+	h4 = 0x510e527f
+	h5 = 0x9b05688c
+	h6 = 0x1f83d9ab
+	h7 = 0x5be0cd19
+
+	ml = len(toBinary(OgMessage)[0])
+
+	k = [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	   0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	   0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	   0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	   0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	   0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	   0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	   0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+
+	#pre-processing:
+	message = message * 2 + 1
+	currentLength = ml + 1
+	while currentLength%512 != 448:
+		message = message * 2
+		currentLength += 1
+	for i in range(64-(len(bin(ml))-2)):
+		message = message * 2
+	message = int(bin(message)[2:] + bin(ml)[2:], 2)
+
+
+
+	#process the message in successive 512-bit chunks:
+	stringMessage = "0" + bin(message)[2:]
+	for i in range((len(bin(message))-1)//512):	
+		chunk = stringMessage[512*i:512*(i+1)]
+		
+		w = [int(chunk[32*j:32*(j+1)], 2) for j in range(16)]
+		
+		for j in range(16, 64):
+			s0 = rightRotate(w[j-15], 7, 32) ^ rightRotate(w[j-15], 18, 32) ^ (w[j-15] >> 3)
+			s1 = rightRotate(w[j-2], 17, 32) ^ rightRotate(w[j-2], 19, 32) ^ (w[j-2] >> 10)
+			newWord = (w[j-16] + s0 + w[j-7] + s1)%(2**32)
+			w.append(newWord)
+		
+		a = h0
+		b = h1
+		c = h2
+		d = h3
+		e = h4
+		f = h5
+		g = h6
+		h = h7
+		
+		for r in range(64):
+			S1 = rightRotate(e, 6, 32) ^ rightRotate(e, 11, 32) ^ rightRotate(e, 25, 32)
+			ch = (e & f) ^ ((~e) & g)
+			temp1 = (h + S1 + ch + k[r] + w[r])%(2**32)
+			S0 = rightRotate(a, 2, 32) ^ rightRotate(a, 13, 32) ^ rightRotate(a, 22, 32)
+			maj = (a & b) ^ (a & c) ^ (b & c)
+			temp2 = (S0 + maj)%(2**32)
+			
+			h = g
+			g = f
+			f = e
+			e = (d + temp1)%(2**32)
+			d = c
+			c = b
+			b = a
+			a = (temp1 + temp2)%(2**32)
+			
+		h0 = (h0 + a)%(2**32)
+		h1 = (h1 + b)%(2**32)
+		h2 = (h2 + c)%(2**32)
+		h3 = (h3 + d)%(2**32)
+		h4 = (h4 + e)%(2**32)
+		h5 = (h5 + f)%(2**32)
+		h6 = (h6 + g)%(2**32)
+		h7 = (h7 + h)%(2**32)
+		
+
+
+	hh = (h0 << 224) | (h1 << 192) | (h2 << 160) | (h3 << 128) | (h4 << 96) | (h5 << 64) | (h6 << 32) | h7
+	return hex(hh)
+	
+message = input("Please input message to get sha256 hash:\n")
+print(f"The sha256 hash of {message} is:\n{sha256(message)}")
